09dd405d8918569946e56e9dc53eca498ec81ef2
Milan Dobrota <milan@milandobrota.com>
2014-08-17 20:07:25 -0400

    Initial Commit

---
+++ pomodoro/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  pomodoro
+//
+//  Created by Milan Dobrota on 8/17/14.
+//  Copyright (c) 2014 Milan Dobrota. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ pomodoro/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  pomodoro
+//
+//  Created by Milan Dobrota on 8/17/14.
+//  Copyright (c) 2014 Milan Dobrota. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
